2e7e4e2d84b76afd6879cdd1535c908aa91e6bd5
ziyanwould <448736378@qq.com>
2019-03-06 17:58:26 +0800

    还差1个版块样式

--- src/pages/zz71/index.js
+++ src/pages/zz71/index.js
@@ -1,20 +1,18 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './common.scss'
+import './index.scss'
 import $ from 'jquery';
 import 'bootstrap/dist/js/bootstrap.js'
 
+
 $("#myCarousel").carousel('cycle');
 
-// export const navs = (x,y,z)=>{
-//     let num = x+y;
-//     num =num*z;
-//     return num;
-// }
-function navs(x,y,z){
+export const navs = (x,y,z)=>{
     let num = x+y;
     num =num*z;
     return num;
 }
+
 
 
